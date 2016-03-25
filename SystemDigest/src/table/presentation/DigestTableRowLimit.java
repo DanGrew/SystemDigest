f5e6ed67b8ff1f0f38d2691f0fdc65a0791a2662
DanGrew <danielanthonygrew@gmail.com>
2016-03-25 20:08:32 +0000

    ISSUE#10 DIGEST CONTROLS:
    Added enum for limiting row size in digest table.

---
+++ SystemDigest/src/table/presentation/DigestTableRowLimit.java
@@ -0,0 +1,71 @@
+/*
+ * ----------------------------------------
+ *             System Digest
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package table.presentation;
+
+import javafx.collections.ObservableList;
+
+/**
+ * The {@link DigestTableRowLimit} is responsible for limiting the {@link table.model.DigestTable}
+ * rows to a certain, configured amount.
+ */
+public enum DigestTableRowLimit {
+   
+   OneHundred( 100 ),
+   FiveHundred( 500 ),
+   OneThousand( 1000 ),
+   FiveThousand( 5000 ),
+   TenThousand( 10000 ),
+   Unlimited( null );
+   
+   static final String UNLIMITED = "Unlimited";
+   private final Integer limit;
+   private final String readable;
+   
+   /**
+    * Constructs a new {@link DigestTableRowLimit}.
+    * @param limit the max number of rows.
+    */
+   private DigestTableRowLimit( Integer limit ) {
+      this.limit = limit;
+      if ( limit == null ) {
+         readable = UNLIMITED; 
+      } else {
+         readable = limit.toString();
+      }
+   }//End Constructor
+
+   /**
+    * Getter for the max number of rows.
+    * @return the limit, can be null for unlimited.
+    */
+   Integer getLimit(){
+      return limit;
+   }//End Method
+   
+   /**
+    * Method to limit the given {@link ObservableList} to the associated limit.
+    * @param listToLimit the {@link ObservableList} to limit.
+    */
+   public void limit( ObservableList< ? > listToLimit ) {
+      if ( limit == null ) return;
+      
+      if ( listToLimit.size() <= limit ) return;
+      
+      listToLimit.remove( limit, listToLimit.size() );
+   }//End Method
+   
+   /**
+    * Method to get the readable form of this limit.
+    * @return the readable, displayable form.
+    */
+   public String readable(){
+      return readable;
+   }//End Method
+
+}//End Enum
---
+++ SystemDigest/test/table/presentation/DigestTableRowLimitTest.java
@@ -0,0 +1,57 @@
+/*
+ * ----------------------------------------
+ *             System Digest
+ * ----------------------------------------
+ *          Produced by Dan Grew
+ *                 2016
+ * ----------------------------------------
+ */
+package table.presentation;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * {@link DigestTableRowLimit} test.
+ */
+public class DigestTableRowLimitTest {
+
+   @Test public void readableShouldBeToStringOfLimit() {
+      for ( DigestTableRowLimit limit : DigestTableRowLimit.values() ) {
+         if ( limit.getLimit() == null ) {
+            assertThat( limit.readable(), is( DigestTableRowLimit.UNLIMITED ) );
+         } else {
+            assertThat( limit.readable(), is( limit.getLimit().toString() ) );
+         }
+      }
+   }//End Method
+   
+   @Test public void limitShouldReduceTheNumberOfItemsFromTheBottom() {
+      for ( DigestTableRowLimit limit : DigestTableRowLimit.values() ) {
+         ObservableList< String > list = FXCollections.observableArrayList();
+         for ( int i = 0; i < 20000; i++ ) {
+            list.add( "" + i );
+         }
+         
+         limit.limit( list );
+         
+         Integer limitValue = limit.getLimit();
+         if ( limitValue == null ) {
+            assertThat( list, hasSize( 20000 ) );
+         } else {
+            int limitIndex = limitValue - 1;
+            assertThat( list, hasSize( limitValue ) );
+            
+            assertThat( list.get( 0 ), is( "0" ) );
+            assertThat( list.get( limitIndex ), is( "" + limitIndex ) );
+         }
+      }
+   }//End Method
+
+}//End Class
